feead2689eaba3cefa07d2b7b73eb9100cd583d3
Dibu Plays YT <dibuplays21@gmail.com>
2026-09-17 10:02:43 -0300

    Update src/dock-widget.hpp for v0.3

--- src/dock-widget.hpp
+++ src/dock-widget.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "canvas-service.hpp"
+#include "output-service.hpp"
 #include "settings-store.hpp"
 
 #include <QWidget>
@@ -8,8 +9,15 @@
 class QCheckBox;
 class QComboBox;
 class QLabel;
+class QLineEdit;
+class QListWidget;
 class QPushButton;
 class QSpinBox;
+class QTimer;
+
+namespace dibu {
+class PreviewWidget;
+}
 
 namespace dibu {
 
@@ -26,15 +34,24 @@ private:
   void buildUi();
   void refreshMasterScenes();
   void refreshCanvasScenes();
+  void refreshCanvasSources();
   void updateStatus();
   void createCanvasScene();
   void linkSelectedScenes();
   void unlinkSelectedMaster();
   void applyCanvasSettings();
+  void activateSelectedCanvasScene();
+  void addExistingSource();
+  void removeSelectedSource();
+  void moveSelectedSource(bool up);
+  void toggleSelectedSourceVisibility();
+  void toggleRecording();
+  void toggleStreaming();
   void persist();
   [[nodiscard]] std::string currentMainSceneName() const;
 
   CanvasService canvas_;
+  OutputService outputs_;
   SettingsStore store_;
   PluginSettings settings_;
   bool initialized_ = false;
@@ -45,8 +62,16 @@ private:
   QSpinBox *heightSpin_ = nullptr;
   QComboBox *masterSceneCombo_ = nullptr;
   QComboBox *canvasSceneCombo_ = nullptr;
+  PreviewWidget *preview_ = nullptr;
+  QListWidget *sourceList_ = nullptr;
   QLabel *linkSummary_ = nullptr;
   QPushButton *applyButton_ = nullptr;
+  QPushButton *recordButton_ = nullptr;
+  QPushButton *streamButton_ = nullptr;
+  QLabel *outputStatus_ = nullptr;
+  QLineEdit *serverEdit_ = nullptr;
+  QLineEdit *keyEdit_ = nullptr;
+  QTimer *statusTimer_ = nullptr;
 };
 
 } // namespace dibu
